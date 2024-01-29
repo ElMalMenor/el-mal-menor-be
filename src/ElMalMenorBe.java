e9ef488fa5c8f02a8a16fbbaac357a2012c3946e
BrendaHab <44091078+BrendaHab@users.noreply.github.com>
2024-01-29 11:49:51 -0300

    Update ElMalMenorBe.java

--- src/ElMalMenorBe.java
+++ src/ElMalMenorBe.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.el.mal.menor.be;
 
-/**
- *
- * @author brenh
- */
 public class ElMalMenorBe {
 
     public static void main(String[] args) {
