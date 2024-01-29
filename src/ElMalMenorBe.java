87aea298479a37ebebfb744ff80fef4982ebbd60
BrendaHab <brendahaberkonb@gmail.com>
2024-01-29 11:41:45 -0300

    Basic structure

---
+++ src/ElMalMenorBe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.el.mal.menor.be;
+
+/**
+ *
+ * @author brenh
+ */
+public class ElMalMenorBe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
